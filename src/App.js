49b70ae54756c56696837cb4f4ac78fa73e7dd6a
Noel Kujur <lovenoelkujur@gmail.com>
2024-06-16 12:46:30 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -21,7 +21,8 @@ const App = () => {
 
     const fetchUser = async() => {
       try {
-        const response = await axios.get("https://hospital-management-system-backend-edeu.onrender.com/api/v1/user/patient/me", {withCredentials : true});
+        const response = await axios.get("https://hospital-management-system-backend-edeu.onrender.com/api/v1/user/patient/me",
+           {withCredentials : true});
         setIsAuthenticated(true);
         setUser(response.data.user);
       } 
